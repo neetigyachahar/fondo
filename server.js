c81cbf3dc174751196f4b17e32081af3891cd801
Neetigya Chahar <neetigyachahar784@gmail.com>
2019-06-13 14:44:59 +0530

    make it better

--- server.js
+++ server.js
@@ -41,24 +41,24 @@ http.createServer(function(req,res){
         if(req.url == '/fileupload'){
             var form = new formidable.IncomingForm();
             form.parse(req, function(err, fields, files){
-                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                 console.log(fields);
                 if((files.filetoupload.type).split('/')[0] == "image"){
                 con.query("select SrNo from PhotoBase where SrNo = (select MAX(SrNo) from PhotoBase)", function(err, result){
-                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                     var oldname = files.filetoupload.path;
                     var newname = __dirname+(parseInt(result[0].SrNo)+1)+'.'+(files.filetoupload.type).split('/')[1] ;
                     fs.rename(oldname, newname, function(err){
-                        if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                        if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                         que = `SELECT userPassword FROM PhotoBase WHERE UserID = ${fields.UserID}`;
                         con.query(que, function(err, resultz){
-                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                         if(toString(resultz) == '[]'){
                             console.log(resultz);
                             var passwd = resultz[0].userPassword;
                             var uploadSql = `insert into PhotoBase (UserID,upvotes, link, PhotoPrivacy, userPassword, tags) values ( ${fields.UserID}, 0,"https://${host}/images/${(parseInt(result[0].SrNo)+1)+'.'+(files.filetoupload.type).split('/')[1]}","Public", "${passwd}" , '${fields.tags}')`;
                             con.query(uploadSql, function(err, result){
-                                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                                 res.writeHead(200);
                                 res.write("<script>window.location.href = 'https://'"+host+"'/profile.html';</script>" );
                                 res.end();
@@ -70,7 +70,7 @@ http.createServer(function(req,res){
                             var passwd = fields.Password;
                             var uploadSql = `insert into PhotoBase (UserID,upvotes, link, PhotoPrivacy, SetWallpaper, userPassword, tags) values ( ${fields.UserID}, 0,"https://${host}/images/${(parseInt(result[0].SrNo)+1)+'.'+(files.filetoupload.type).split('/')[1]}","Public", 0, "${passwd}" , '${fields.tags}')`;
                             con.query(uploadSql, function(err, result){
-                                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                                 res.writeHead(200);
                                 res.write(`<script>window.location.href = 'https://${host}/profile.html'</script>` );
                                 res.end();
@@ -86,7 +86,7 @@ http.createServer(function(req,res){
         }
         else{
             fs.readFile('profile.html',function(err, data){
-                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                     res.writeHead(200);
                     res.write("<script>alert('Wrong file format!');</script>");
                     res.write(data);
@@ -100,7 +100,7 @@ http.createServer(function(req,res){
         }
         else if(req.url == '/create'){
             fs.readFile("create.html", function(err, result){
-                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                 res.writeHead(200);
                 res.write(result);
                 res.end();
@@ -110,7 +110,7 @@ http.createServer(function(req,res){
         else{
         fs.readFile(req.url.slice(1), function(err, bit) 
         {
-            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
             res.writeHead(200);
             res.write(bit);
             res.end();
@@ -124,7 +124,7 @@ http.createServer(function(req,res){
     console.log(Object.keys(ki)[0]);
     switch (Object.keys(ki)[0]) {
         case 'search':{fs.readFile('SearchResult.html','utf8', function(err, file){
-                        if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                        if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                         var obj = {
                             'html': file,
                             'UserID': null,
@@ -133,12 +133,12 @@ http.createServer(function(req,res){
                         console.log(`SELECT * FROM PhotoBase WHERE UserID = "${ki.search}"`);
                         console.log("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
                         con.query(`SELECT * FROM PhotoBase WHERE UserID = ${ki.search} AND PhotoPrivacy = 'Public'`, function(err, result){
-                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                             console.log("__________________________"+(result)+"sfsf");
                             if((result).length == 0){
                                 console.log("fdzvfmofvnajfnv+++++++++++++++++++++++++");
                                 con.query(`SELECT COUNT(*) FROM PhotoBase WHERE UserID = ${ki.search}`, function(err, result){
-                                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                                     if(result[0]['COUNT(*)'] > 0 ){
                                         obj.UserID = ki.search;
                                     }
@@ -169,7 +169,7 @@ http.createServer(function(req,res){
             console.log(`SELECT * FROM photobase WHERE UserID = ${ki.profile}`);
             con.query(`SELECT * FROM photobase WHERE UserID = ${ki.profile}`, function(err, result){
                 console.log("3333333333");
-                if (err)         {    console.log("444444444444");         res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {    console.log("444444444444");         res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                     console.log("55555555555555");
                     console.log(result);
                 if(result.length === 0){
@@ -197,7 +197,7 @@ http.createServer(function(req,res){
             var que = `SELECT userPassword FROM PhotoBase WHERE UserID = ${parseInt(ki.password[0])}`;
             console.log(que);
             con.query(que, function(err, results){
-                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                 var obj = {
                     "auth": null
                 }
@@ -218,7 +218,7 @@ http.createServer(function(req,res){
         case "delete":{
             var que = `DELETE FROM PhotoBase WHERE SrNo = ${ki.delete}`;
             con.query(que, function(err, result){
-                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                 if (result.affectedRows == 1){
                     res.writeHead(200);
                     res.write(JSON.stringify({"del":true}));
@@ -237,18 +237,18 @@ http.createServer(function(req,res){
         case "togglePrivacy" : {
             var que = `SELECT * FROM PhotoBase WHERE SrNo = ${ki.togglePrivacy}`;
             con.query(que, function(err, result){
-                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                 var privacy = "Only me";
                 if (result[0].PhotoPrivacy == "Only me"){
                     privacy = "Public";
                 }
                 que = `UPDATE PhotoBase SET PhotoPrivacy = "${privacy}" WHERE SrNo = ${ki.togglePrivacy}`;
                 con.query(que, function(err, result){
-                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                     if (result.affectedRows == 1){
                         que = `SELECT PhotoPrivacy, uploaded_at FROM PhotoBase WHERE SrNo = ${ki.togglePrivacy}`;
                         con.query(que, function(err, result){
-                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                             res.writeHead(200);
                             res.write(JSON.stringify(result));
                             res.end();
@@ -269,14 +269,14 @@ http.createServer(function(req,res){
         case "setback" : {
             var que = `UPDATE PhotoBase SET SetWallpaper = 0 WHERE UserID = ${ki.UserID} AND SetWallpaper = 1`
             con.query(que, function(err){
-                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                 que = `SELECT * FROM PhotoBase WHERE SrNo = ${ki.setback}`
                 con.query(que, function(err, result){
-                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                    if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                     if(result[0].UserID == ki.UserID){
                         que = `UPDATE PhotoBase SET SetWallpaper = 1 WHERE SrNo = ${ki.setback}`
                         con.query(que, function(err, result1){
-                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                             res.writeHead(200);
                             res.write(JSON.stringify({'reply':true}));
                             res.end();
@@ -286,13 +286,13 @@ http.createServer(function(req,res){
                     else{
                         que = `SELECT userPassword FROM PhotoBase WHERE UserID = ${ki.UserID}`
                         con.query( que , function(err, pass){
-                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                            if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                             console.log(result[0].uploaded_at);
                             var t = result[0].uploaded_at;
                             console.log(t.toUTCString());
                             que = `INSERT INTO PhotoBase (UserID, link, upvotes, PhotoPrivacy, SetWallpaper, userPassword, tags ) values (${ki.UserID}, "${result[0].link}", ${result[0].upvotes}, "${result[0].PhotoPrivacy}", 1, "${pass[0].userPassword}", "${result[0].tags}")`;
                             con.query(que, function(err, abcd){
-                                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                                 res.writeHead(200);
                                 res.write(JSON.stringify({'reply':true}));
                                 res.end();
@@ -310,7 +310,7 @@ http.createServer(function(req,res){
         case "checkID" : {
             var que = `SELECT COUNT(*) FROM PhotoBase WHERE UserID = ${ki.checkID}`;
             con.query(que, function(err, result){
-                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();  throw err;         }else{
+                if (err)         {             res.writeHead(404);             res.write("some error occurred");             res.end();         }else{
                 var bool;
                 if(result[0]["COUNT(*)"] != 0){
                     bool = 'true';
